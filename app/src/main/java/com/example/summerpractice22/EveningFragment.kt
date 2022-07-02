21413d949076b1a3827762718d7d7eb9e91bb1b7
Alina123456 <vaskina.alina.ru@mail.ru>
2022-07-02 22:22:11 +0300

    Added hw2

--- app/src/main/java/com/example/summerpractice22/EveningFragment.kt
+++ app/src/main/java/com/example/summerpractice22/EveningFragment.kt
@@ -3,9 +3,37 @@ package com.example.summerpractice22
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.summerpractice22.databinding.FragmentEveningBinding
 
 class EveningFragment : Fragment(R.layout.fragment_evening) {
+
+    private var _binding: FragmentEveningBinding? = null
+    private val binding get() = _binding!!
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        _binding = FragmentEveningBinding.bind(view)
+
+        with(binding) {
+            btnTimeEvening.setOnClickListener {
+                val bundle = Bundle()
+                val text = "EveningFragment"
+                bundle.putString(
+                    "ARG_TEXT",
+                    text
+                )
+                findNavController().navigate(
+                    R.id.action_eveningFragment_to_timeFragment,
+                    bundle,
+                )
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
--- app/src/main/java/com/example/summerpractice22/NightFragment.kt
+++ app/src/main/java/com/example/summerpractice22/NightFragment.kt
@@ -3,9 +3,37 @@ package com.example.summerpractice22
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.summerpractice22.databinding.FragmentNightBinding
 
 class NightFragment : Fragment(R.layout.fragment_night) {
+
+    private var _binding: FragmentNightBinding? = null
+    private val binding get() = _binding!!
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        _binding = FragmentNightBinding.bind(view)
+
+        with(binding) {
+            btnTimeNight.setOnClickListener {
+                val bundle = Bundle()
+                val text = "NightFragment"
+                bundle.putString(
+                    "ARG_TEXT",
+                    text
+                )
+                findNavController().navigate(
+                    R.id.action_nightFragment_to_timeFragment,
+                    bundle,
+                )
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
